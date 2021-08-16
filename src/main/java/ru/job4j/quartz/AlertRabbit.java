952704612d75522b234c4d75aa33e201a85a8aac
Evgeny Novoselov <zkodru@gmail.com>
2021-08-16 14:20:44 +0300

    Сделал настройку интервала из файла rabbit.properties

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,14 +3,30 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class AlertRabbit {
+
+    private static Properties properties;
+
+    private static void init() {
+        properties = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
+        init();
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = JobBuilder.newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = SimpleScheduleBuilder.simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(Integer.parseInt(properties.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = TriggerBuilder.newTrigger()
                     .startNow()
