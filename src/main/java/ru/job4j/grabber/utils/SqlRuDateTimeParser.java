ece0bc725b870044f37d7a29a8cca57b0c5a43d0
Evgeny Novoselov <zkodru@gmail.com>
2021-08-18 16:18:16 +0300

    Сделал разбор даты по патерну

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -1,25 +1,30 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
     private static final Map<String, String> MONTHS = Map.ofEntries(
-            Map.entry("янв", "1"),
-            Map.entry("фев", "2"),
-            Map.entry("мар", "3"),
-            Map.entry("апр", "4"),
-            Map.entry("май", "5"),
-            Map.entry("июн", "6"),
-            Map.entry("июл", "7"),
-            Map.entry("авг", "8"),
-            Map.entry("сен", "9"),
+            Map.entry("янв", "01"),
+            Map.entry("фев", "02"),
+            Map.entry("мар", "03"),
+            Map.entry("апр", "04"),
+            Map.entry("май", "05"),
+            Map.entry("июн", "06"),
+            Map.entry("июл", "07"),
+            Map.entry("авг", "08"),
+            Map.entry("сен", "09"),
             Map.entry("окт", "10"),
             Map.entry("ноя", "11"),
             Map.entry("дек", "12")
     );
+
     @Override
     public LocalDateTime parse(String parse) {
-        return null;
+        String str = "30 07 21, 10:35";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yy, HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
+        return dateTime;
     }
 }
