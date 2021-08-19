0bba91c61e6d8de923b4fef218416205f417b320
Evgeny Novoselov <zkodru@gmail.com>
2021-08-19 15:41:42 +0300

    2.4. SqlRuParse [#285213] (#10)

---
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -0,0 +1,66 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.model.Post;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
+
+    private static int postCounterId;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        Document document;
+        try {
+            document = Jsoup.connect(link).get();
+            Elements row = document.select(".postslisttopic");
+            int skip = 3;
+            for (Element element : row) {
+                if (skip > 0) {
+                    skip--;
+                    continue;
+                }
+                Element href = element.child(0);
+                posts.add(detail(href.attr("href")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Document document;
+        Post post = null;
+        try {
+            document = Jsoup.connect(link).get();
+            String title = document.title().substring(0, document.title().indexOf(" / Вакансии"));
+            String time = document.select(".msgFooter").get(0).text();
+            time = time.substring(0, time.indexOf(" ["));
+            post = new Post(
+                    title,
+                    link,
+                    document.select(".msgBody").get(1).text(),
+                    dateTimeParser.parse(time)
+            );
+            post.setId(++postCounterId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+}
