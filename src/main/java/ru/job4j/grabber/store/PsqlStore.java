2c084b6901693cdbc2d0f9e48bbc9ffb44e9eb9e
Evgeny Novoselov <zkodru@gmail.com>
2021-08-20 12:56:50 +0300

    Реализовал конструктор PsqlStore и закрытие ресурсов и сохранение поста

--- src/main/java/ru/job4j/grabber/store/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/store/PsqlStore.java
@@ -1,4 +1,85 @@
 package ru.job4j.grabber.store;
 
-public class PsqlStore {
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.parse.Parse;
+import ru.job4j.grabber.parse.SqlRuParse;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+    private Connection cn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            cn = DriverManager.getConnection(
+                    cfg.getProperty("jdbc.url"),
+                    cfg.getProperty("jdbc.username"),
+                    cfg.getProperty("jdbc.password")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement = cn
+                .prepareStatement("into insert post(name, text, link, created) values (?,?,?,?)", Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    post.setId(resultSet.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        return null;
+    }
+
+    @Override
+    public Post findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (PsqlStore store = new PsqlStore(properties)) {
+            Parse parse = new SqlRuParse(new SqlRuDateTimeParser());
+            List<Post> posts = parse.list("https://www.sql.ru/forum/job-offers");
+            posts.forEach(store::save);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
