261ba059130e41a0cdf7cf4b231015331806a171
Evgeny Novoselov <zkodru@gmail.com>
2021-08-20 15:03:24 +0300

    Написал основные методы

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -1,4 +1,68 @@
 package ru.job4j.grabber;
 
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.parse.Parse;
+import ru.job4j.grabber.parse.SqlRuParse;
+import ru.job4j.grabber.store.Store;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Grabber implements Grab {
+    private final Properties cfg = new Properties();
+
+    public Store store() {
+        return null;
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        try (InputStream in = Grabber.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            cfg.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = JobBuilder.newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("rabbit.interval")))
+                .repeatForever();
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            // TODO logic
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new SqlRuParse(new SqlRuDateTimeParser()), store, scheduler);
+    }
 }
