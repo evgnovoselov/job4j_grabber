ea006e9d2b90e0ae0b879a6e801c8489e91a8be2
Evgeny Novoselov <zkodru@gmail.com>
2021-08-20 11:59:02 +0300

    Добавил схему таблицы post

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,11 @@
 create table if not exists rabbit (
     id serial primary key,
     created_date timestamp
-)
+);
+create table if not exists post (
+    id serial primary key,
+    name varchar(255),
+    text text,
+    link varchar(255) unique,
+    created timestamp
+);
